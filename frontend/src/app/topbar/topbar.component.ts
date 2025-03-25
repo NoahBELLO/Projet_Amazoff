e9ca51ca47771f8868e19e942acb0a73a9cb1022
florianpotier-clemente <florian.potier-clemente@e-cdp.com>
2025-03-25 11:54:13 +0100

    <chores>💩 **Dashboard & topbar** modification apportée a la top bar et au dashboard pour commencer à y inclure les chemins menant au compte utilisateur, permettant d'ajouter des articles, de modifier les paramètres, de gérer le magasin, etc.

--- frontend/src/app/topbar/topbar.component.ts
+++ frontend/src/app/topbar/topbar.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
+  imports: [RouterLink],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
