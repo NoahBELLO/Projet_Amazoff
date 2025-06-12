3399b7d371fabdf626a8ef6045cbd335b7323e1d
florianpotier-clemente <florian.potier-clemente@e-cdp.com>
2025-06-12 14:01:31 +0200

    <chores> **Commandes** possibilité de rejoindre la page d'un article en cliquant sur l'image de ce dernier ou sa description

--- frontend/src/app/commande-bloc/commande-bloc.component.ts
+++ frontend/src/app/commande-bloc/commande-bloc.component.ts
@@ -1,10 +1,11 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, Input, ViewChild } from '@angular/core';
 import { ArticleRatingModalComponent } from '../article-rating-modal/article-rating-modal.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-commande-bloc',
-  imports: [NgFor, NgIf, ArticleRatingModalComponent],
+  imports: [NgFor, NgIf, ArticleRatingModalComponent, RouterLink],
   templateUrl: './commande-bloc.component.html',
   styleUrl: './commande-bloc.component.css'
 })
