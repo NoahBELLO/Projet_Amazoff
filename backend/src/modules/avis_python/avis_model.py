38e0e21f4ab376ab90920f9188a1c052dd43c514
florianpotier-clemente <florian.potier-clemente@e-cdp.com>
2025-06-20 15:59:24 +0200

    <feat> **Avis** Restructuration d'avis en cours pour le model MONGO ce dernier est opérationnel reste à le faire pour MARIADB

--- backend/src/modules/avis_python/avis_model.py
+++ backend/src/modules/avis_python/avis_model.py
@@ -1,5 +1,5 @@
 import time
-from mongoengine import Document, StringField, IntField, DateTimeField, FloatField, ListField
+from mongoengine import Document, StringField, IntField, DateTimeField, FloatField, ListField, LongField
 from bson import ObjectId
 from tools.customeException import ErrorExc
 from loguru import logger
@@ -7,10 +7,13 @@ from user_python.user_model import UserModel
 
 
 class AvisModel(Document): 
+    id_maria = IntField(required=True)
     article_id = StringField(required=True)
-    evaluations = IntField(required=False)
-    stars = FloatField(required=False)
-    comments = ListField(required=True)
+    comment = StringField(required=False)
+    name = StringField(required=True)
+    fname = StringField(required=True)
+    stars = FloatField(required=True)
+    date_publication = StringField(required=True)
 
     meta = {'collection': 'avis' , 'db_alias': 'avis-db'}
 
@@ -18,68 +21,57 @@ class AvisModel(Document):
         """Convertir un document en dictionnaire"""
         return {
             "id": str(self.id),  
+            "id_maria": int(self.id_maria),
             "article_id": self.article_id,
-            "evaluations": self.evaluations,
-            "comments": self.comments,
-            "stars": self.stars
+            "comment": self.comment,
+            "name" : self.name,
+            "fname": self.fname,
+            "stars": self.stars,
+            "date_publication": self.date_publication
         }
     
     def check_fields(self, datas):
-        comments = datas['comments']
-        if "stars" not in comments or int(comments['stars']) == 0:
+        if "stars" not in datas or int(datas['stars']) == 0:
             raise ErrorExc(f"Veuillez évaluer l'article.")
                 
         if "article_id" not in datas or len(str(datas['article_id'])) == 0 :
             raise ErrorExc(f"Article non fourni.")
         
-        if "user_id" not in comments or len(str(comments['user_id'])) == 0 :
+        if "user_id" not in datas or len(str(datas['user_id'])) == 0 :
             raise ErrorExc(f"Utilisateur non reconnu.")
 
 
-    def rating_article(self, datas):
-            #datas = article_id, rating, user_id, comment
+    def rating_article(self, datas, id_maria):
             self.check_fields(datas)
             article_id = str(datas['article_id'])
-            comments = datas['comments']
-            stars = float(comments['stars'])
+            comment = datas['comment']
+            stars = float(datas['stars'])
 
             #ajout de l'utilisateur au données de son commentaire
-            user = UserModel.objects(id=comments['user_id']).first().to_dict()
+            user = UserModel.objects(id=datas['user_id']).first()
             if user == None:
                 raise ErrorExc("Utilisateur inconnu")
-            
-            DATETIME = time.strftime('%d-%m-%Y-%H-%M-%S')
-            comments['fname'] = user['fname']
-            comments['name'] = user['name']
-            comments['date_publication'] = DATETIME
+            else:
+                user = user.to_dict()
 
+            DATETIME = time.strftime('%d-%m-%Y-%H-%M-%S')
+            fname = user['fname']
+            name = user['name']
+            date_publication = DATETIME
+            avis = AvisModel(
+                article_id=article_id,
+                id_maria=id_maria,
+                comment=comment,
+                stars=stars,
+                fname=fname,
+                name=name,
+                date_publication=date_publication
+            )
+            avis2 = avis.to_dict()
+            logger.critical(avis2)
             try:
-                avis = AvisModel.objects(article_id=article_id).first()  
-                #cas premier avis
-                if not avis:
-                    avis = AvisModel(
-                        article_id=article_id,
-                        stars=stars,
-                        evaluations=1,
-                        comments=[comments] 
-                    )
-                    avis.save()
-                    return True
-                #cas tableau comments vide uniquement
-                if len(avis.comments) < 0:
-                    avis = AvisModel(
-                        stars=stars,
-                        evaluations=1,
-                        comments=[comments] 
-                    )
-                    avis.save()
-                    return True
-                else:
-                    avis.stars = (float(avis.stars) * int(avis.evaluations) + stars) / (avis.evaluations + 1)
-                    avis.comments.append(comments)
-                    avis.save()
-                    return True
-                
+                avis.save()
+                return True, str(avis.id)
             except ErrorExc as e:
                 logger.error(f"Erreur lors du save : {e}")
                 raise ErrorExc("Erreur lors de l'évaluation")
--- backend/src/modules/avis_python/avis_routes.py
+++ backend/src/modules/avis_python/avis_routes.py
@@ -1,9 +1,13 @@
 import os
 from datetime import datetime, timezone
+from bson import ObjectId
 from flask import Blueprint, jsonify, request
 from loguru import logger
 from avis_python.avis_model import AvisModel
 from articles_python.articles_model import ArticleModel
+from articles_python.articles_bdd import TableArticles
+from panier_python.panier_model import PanierModel, PanierModelMD
+from tools.db_health import disable_maria, disable_mongo, test_maria, test_mongo
 from tools.customeException import ErrorExc
 
 LOG_FILE = os.path.join(os.path.dirname(__file__), 'failed_insert_articles.log')
@@ -22,6 +26,52 @@ def log_failure(target: str, datas: dict, error: Exception):
 #route patch
 @bp.route("/rating_article", methods=["PATCH"])
 def rate_article():
+    response = {"ids": {}}
+    logger.critical('route create avis')
+    datas = request.json
+    mongo_id = False
+    id_maria = TableArticles().getLastId()
+    logger.critical(datas)
+
+  # --- MongoDB ---
+    if test_mongo():
+        try:
+            db = AvisModel()
+            err_mongo, mongo_id = db.rating_article(datas, id_maria + 1)
+            if err_mongo:
+                response["ids"]["mongo"] = str(mongo_id)
+            else:
+                raise RuntimeError("Erreur lors de la création du panier sur MongoDB")
+        except ErrorExc as e:
+            logger.warning(f"[CREATE_CART] Échec MongoDB : {e}")
+            log_failure('MONGO_CART', {"user_id": datas['user_id']}, e)
+            disable_mongo()
+
+#  # --- MariaDB ---
+#     if test_maria():
+#         try:
+#             if not mongo_id:
+#                 mongo_id = ObjectId()
+#                 logger.critical(f"[CREATE_CART] MongoId généré : {mongo_id}")
+            
+#             datas_maria = {"articles": [], "user_id": user_id, "id": mongo_id}
+#             db2 = PanierModelMD()
+#             err_maria, maria_id = db2.rating_article(datas_maria)
+#             if err_maria:
+#                 response["ids"]["mariadb"] = maria_id
+#             else:
+#                 raise RuntimeError("Erreur lors de la création du panier sur MariaDB")
+#         except Exception as e:
+#             logger.warning(f"[CREATE_CART] Échec MariaDB : {e}")
+#             log_failure('MARIADB_CART', {"user_id": user_id}, e)
+#             disable_maria()
+
+    # Réponse 
+    if not response["ids"]:
+        return jsonify({"error": True, "message": "Aucun insert n’a fonctionné."})
+    return jsonify({'error': False, 'rs': response})
+    
+    
     try:
         datas = request.json
         db = AvisModel()
